a97e47fa0c7b1869e87b918a3e11729531691d71
Joe Vasquez <joe.vasquez@gmail.com>
2015-03-29 21:53:14 -0500

    pep8

--- vuln_manager/cves/management/commands/load_full_CVE_feed.py
+++ vuln_manager/cves/management/commands/load_full_CVE_feed.py
@@ -10,27 +10,60 @@ from lxml import etree
 import time
 
 
-
 def parse_cves(element, updater):
     updater.add_items(
         Item.objects.filter(
-            cpe22_wfn__in=get_xpath(element, 'a:vulnerable-software-list/a:product/text()')
+            cpe22_wfn__in=get_xpath(
+                element,
+                'a:vulnerable-software-list/a:product/text()'
+            )
         ).values_list('pk', flat=True),
         Vulnerability(
             cve_id=element.get('id'),
-            published=get_xpath_date(element, 'a:published-datetime/text()'),
-            modified=get_xpath_date(element, 'a:last-modified-datetime/text()'),
+            published=get_xpath_date(
+                element, 'a:published-datetime/text()'),
+            modified=get_xpath_date(
+                element, 'a:last-modified-datetime/text()'),
             cwe=get_xpath(element, 'a:cwe/@id', False),
             summary=get_xpath(element, 'a:summary/text()', False),
-            references=[x for x in get_refrences(get_xpath(element, 'a:references/a:reference'))],
-            cvss_base_score=get_xpath(element, 'a:cvss/b:base_metrics/b:score/text()', False),
-            cvss_access_vector=get_xpath(element, 'a:cvss/b:base_metrics/b:access-vector/text()', False),
-            cvss_access_complexity=get_xpath(element, 'a:cvss/b:base_metrics/b:access-complexity/text()', False),
-            cvss_authentication=get_xpath(element, 'a:cvss/b:base_metrics/b:authentication/text()', False),
-            cvss_confidentiality_impact=get_xpath(element, 'a:cvss/b:base_metrics/b:confidentiality-impact/text()', False),
-            cvss_integrity_impact=get_xpath(element, 'a:cvss/b:base_metrics/b:integrity-impact/text()', False),
-            cvss_availability_impact=get_xpath(element, 'a:cvss/b:base_metrics/b:availability-impact/text()', False),
-            cvss_generated=get_xpath_date(element, 'a:cvss/b:base_metrics/b:generated-on-datetime/text()'),
+            references=[x for x in get_refrences(
+                get_xpath(element, 'a:references/a:reference'))],
+            cvss_base_score=get_xpath(
+                element, 'a:cvss/b:base_metrics/b:score/text()', False),
+            cvss_access_vector=get_xpath(
+                element,
+                'a:cvss/b:base_metrics/b:access-vector/text()',
+                False
+            ),
+            cvss_access_complexity=get_xpath(
+                element,
+                'a:cvss/b:base_metrics/b:access-complexity/text()',
+                False
+            ),
+            cvss_authentication=get_xpath(
+                element,
+                'a:cvss/b:base_metrics/b:authentication/text()',
+                False
+            ),
+            cvss_confidentiality_impact=get_xpath(
+                element,
+                'a:cvss/b:base_metrics/b:confidentiality-impact/text()',
+                False
+            ),
+            cvss_integrity_impact=get_xpath(
+                element,
+                'a:cvss/b:base_metrics/b:integrity-impact/text()',
+                False
+            ),
+            cvss_availability_impact=get_xpath(
+                element,
+                'a:cvss/b:base_metrics/b:availability-impact/text()',
+                False
+            ),
+            cvss_generated=get_xpath_date(
+                element,
+                'a:cvss/b:base_metrics/b:generated-on-datetime/text()'
+            ),
             dictionary=updater.dictionary
         )
     )
@@ -57,7 +90,8 @@ class Command(BaseCommand):
         dictionary.save()
         updater = Updater(dictionary, Vulnerability)
 
-        context = etree.iterparse(path, events=('end', ), tag=FEED_SCHEMA + 'entry')
+        context = etree.iterparse(
+            path, events=('end', ), tag=FEED_SCHEMA + 'entry')
         fast_iter(context, parse_cves, updater)
         updater.save()
 
@@ -65,4 +99,3 @@ class Command(BaseCommand):
         end = float(time.time())
         dictionary.duration = end - dictionary.start
         dictionary.save()
-
