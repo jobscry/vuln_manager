191d33ea1b11c5043cbf030945fb9214474ae701
Joe Vasquez <joe.vasquez@gmail.com>
2015-04-17 20:43:39 -0500

    pyflakes, pep8

--- vuln_manager/cpes/management/commands/load_CPE_dict.py
+++ vuln_manager/cpes/management/commands/load_CPE_dict.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.utils.dateparse import parse_datetime
 from django.utils.timezone import now
@@ -7,7 +7,7 @@ from cpes.models import (
 )
 from .utils import Updater, fast_iter, get_remote_dict
 
-from os.path import join, normpath, isfile
+from os.path import join
 from lxml import etree
 
 import time
@@ -26,8 +26,9 @@ NAMESPACE_DICT = {
 
 
 def parse_cpes_update(element, cpe_updater):
-    
-    cpe23_wfn = element.xpath('b:cpe23-item/@name', namespaces=NAMESPACE_DICT)[0]
+
+    cpe23_wfn = element.xpath(
+        'b:cpe23-item/@name', namespaces=NAMESPACE_DICT)[0]
 
     if Item.objects.filter(cpe23_wfn=cpe23_wfn).exists():
         cpe_updater.increment('num_existing')
@@ -41,14 +42,15 @@ def parse_cpes_update(element, cpe_updater):
                 deprecated_by=deprecated.xpath(
                     'b:deprecated-by/@name', namespaces=NAMESPACE_DICT)[0],
                 deprecation_type=deprecated.xpath(
-                'b:deprecated-by/@type', namespaces=NAMESPACE_DICT)[0]
+                    'b:deprecated-by/@type', namespaces=NAMESPACE_DICT)[0]
             )
             cpe_updater.increment('num_deprecated')
 
     else:
 
         cpe22_wfn = element.get('name')
-        titles = element.xpath("a:title[@c:lang='en-US']/text()", namespaces=NAMESPACE_DICT)
+        titles = element.xpath(
+            "a:title[@c:lang='en-US']/text()", namespaces=NAMESPACE_DICT)
         cpe_title = 'No title'
         if len(titles) == 1:
             cpe_title = titles[0]
@@ -60,18 +62,20 @@ def parse_cpes_update(element, cpe_updater):
         defaults['cpe23_wfn'] = cpe23_wfn
         cpe = Item(**defaults)
 
-        deprecated = element.xpath('b:cpe23-item/b:deprecation', namespaces=NAMESPACE_DICT)
+        deprecated = element.xpath(
+            'b:cpe23-item/b:deprecation', namespaces=NAMESPACE_DICT)
         if len(deprecated) == 1:
             deprecated = deprecated[0]
             cpe.deprecated = True
             cpe.deprecation_date = parse_datetime(deprecated.get('date'))
             cpe.deprecated_by = deprecated.xpath(
-                'b:deprecated-by/@name', namespaces=NAMESPACE_DICT)[0]    
+                'b:deprecated-by/@name', namespaces=NAMESPACE_DICT)[0]
             cpe.deprecation_type = deprecated.xpath(
                 'b:deprecated-by/@type', namespaces=NAMESPACE_DICT)[0]
             cpe_updater.increment('num_deprecated')
 
-        cpe.references = element.xpath('a:references/a:reference/@href', namespaces=NAMESPACE_DICT)
+        cpe.references = element.xpath(
+            'a:references/a:reference/@href', namespaces=NAMESPACE_DICT)
 
         cpe_updater.add_item(cpe)
         cpe_updater.increment('num_references', len(cpe.references))
@@ -79,9 +83,11 @@ def parse_cpes_update(element, cpe_updater):
 
 def parse_cpes_full(element, cpe_updater):
     cpe22_wfn = element.get('name')
-    cpe23_wfn = element.xpath('b:cpe23-item/@name', namespaces=NAMESPACE_DICT)[0]
+    cpe23_wfn = element.xpath(
+        'b:cpe23-item/@name', namespaces=NAMESPACE_DICT)[0]
 
-    titles = element.xpath("a:title[@c:lang='en-US']/text()", namespaces=NAMESPACE_DICT)
+    titles = element.xpath(
+        "a:title[@c:lang='en-US']/text()", namespaces=NAMESPACE_DICT)
     cpe_title = 'No title'
     if len(titles) == 1:
         cpe_title = titles[0]
@@ -93,18 +99,20 @@ def parse_cpes_full(element, cpe_updater):
     defaults['cpe23_wfn'] = cpe23_wfn
     cpe = Item(**defaults)
 
-    deprecated = element.xpath('b:cpe23-item/b:deprecation', namespaces=NAMESPACE_DICT)
+    deprecated = element.xpath(
+        'b:cpe23-item/b:deprecation', namespaces=NAMESPACE_DICT)
     if len(deprecated) == 1:
         deprecated = deprecated[0]
         cpe.deprecated = True
         cpe.deprecation_date = parse_datetime(deprecated.get('date'))
         cpe.deprecated_by = deprecated.xpath(
-            'b:deprecated-by/@name', namespaces=NAMESPACE_DICT)[0]    
+            'b:deprecated-by/@name', namespaces=NAMESPACE_DICT)[0]
         cpe.deprecation_type = deprecated.xpath(
             'b:deprecated-by/@type', namespaces=NAMESPACE_DICT)[0]
         cpe_updater.increment('num_deprecated', 1)
 
-    cpe.references = element.xpath('a:references/a:reference/@href', namespaces=NAMESPACE_DICT)
+    cpe.references = element.xpath(
+        'a:references/a:reference/@href', namespaces=NAMESPACE_DICT)
 
     cpe_updater.add_item(cpe)
     cpe_updater.increment('num_references', len(cpe.references))
@@ -114,7 +122,8 @@ class Command(BaseCommand):
     help = 'Populates/Updates the CPE Database'
 
     def add_arguments(self, parser):
-        parser.add_argument('--full',
+        parser.add_argument(
+            '--full',
             action='store_true',
             dest='full',
             default=False,
@@ -171,7 +180,7 @@ class Command(BaseCommand):
                 start=float(time.time()),
                 last_modified=new_created,
                 etag=new_etag
-            )         
+            )
 
             n = next(context)
             update.title = n[1].text
@@ -198,12 +207,12 @@ class Command(BaseCommand):
             context = etree.iterparse(file_path, events=('end', ), tag=tag)
             if options['full']:
                 if self.verbosity >= 2:
-                    self.stdout.write('Full database populate.')    
+                    self.stdout.write('Full database populate.')
                 fast_iter(context, parse_cpes_full, cpe_updater)
             else:
                 if self.verbosity >= 2:
-                    self.stdout.write('Database update only.')    
-                fast_iter(context, parse_cpes_update, cpe_updater)                
+                    self.stdout.write('Database update only.')
+                fast_iter(context, parse_cpes_update, cpe_updater)
 
             cpe_updater.save()
 
